76c58d0e84a04337f5bbc4d05f6c10071c9046eb
Shweta Jhajharia <shwetajhajharia@Shwetas-iMac.local>
2020-10-27 10:31:22 +0000

    added to cart functionality added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 var cart = document.getElementById("cart");
+var courses = document.getElementById("list-courses");
+var tablebody = document.querySelector("#list-cart tbody");
 
-document.getElementById("1").addEventListener("click", function(){
-    cart.style.backgroundColor = "black";
+loadEventListeners();
+function loadEventListeners() {
+    cart.addEventListener('click', function (){
+        alert("hello")
+    })
+courses.addEventListener('click', function(event){
+    if(event.target.classList.contains("add-cart")){
+
+        const course = event.target.parentElement.parentElement;
+        var img =  course.querySelector('img');
+        var CourseType = courses.querySelector('h3').textContent;
+        var price = courses.querySelector('#currentcost').textContent;
+        var id = courses.querySelector('.add-cart').getAttribute("data-id");
+
+        var row = document.createElement("tr");
+        row.innerHTML = `
+        <td>
+            <img src="${img}" width=100/>
+        </td> 
+        <td>${CourseType}</td>
+        <td>${price}</td>
+        <td>
+            <a href="#" class="delete-course" data-id="${id}"></a>
+            </td>
+        `;
+        tablebody.appendChild(row);
+    }
 })
+}
