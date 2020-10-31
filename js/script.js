f27201183b016a8135cb18b6b8b3752b1909891f
Shweta Jhajharia <shwetajhajharia@Shwetas-iMac.local>
2020-10-31 10:32:22 +0000

    localstorage implemented

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 var cart = document.getElementById("cart");
 var courses = document.getElementById("list-courses");
 var tablebody = document.querySelector("#list-cart tbody");
-
+var emptyCartButton = document.getElementById("empty-cart");
 loadEventListeners();
 function loadEventListeners() {
-    cart.addEventListener('click', deleteCourse)
-    courses.addEventListener('click', addCourse)
+    cart.addEventListener('click', deleteCourse);
+    courses.addEventListener('click', addCourse);
+    emptyCartButton.addEventListener('click', emptyCart);
+    document.addEventListener('DOMContentLoaded', readLocalStorage)
 }
 function addCourse(event){
     if(event.target.classList.contains("add-cart")){
@@ -28,6 +30,7 @@ function addCourse(event){
             </td>
         `;
         tablebody.appendChild(row);
+        saveCourseToLocalStorage(courseInfo);
     }
 }
 
@@ -35,4 +38,44 @@ function deleteCourse(event){
     if (event.target.classList.contains("delete-course")){
         event.target.parentElement.parentElement.remove();
     }
+}
+localStorage.clear();
+function emptyCart() {
+    while (tablebody.firstChild){
+        tablebody.removeChild(tablebody.firstChild);
+    }
+}
+
+function saveCourseToLocalStorage(course){
+    let courses = getCoursesLocalStorage();
+    courses.push(course);
+    localStorage.setItem('courses', JSON.stringify(course));
+}
+
+function readLocalStorage(){
+    let courses = getCoursesLocalStorage;
+    console.log(courses);
+    courses.forEach(function(course){
+        let row = document.createElement('tr');
+        row.innerHTML = `
+        <td><img src="${course.img}" /></td>
+        <td>${course.courseType}</td>
+        <td>${course.price}</td>
+        <td>
+            <a href="#" class="delete-course" data-id="${course.id}">X</a>
+            </td>
+        `;
+        tablebody.appendChild(row);
+    })
+   
+}
+
+function getCoursesLocalStorage(){
+    let courses;
+    if (localStorage.getItem('courses')== null){
+        courses=[];
+    }else{
+        courses = JSON.parse(localStorage.getItem('courses'));
+    }
+    return courses;
 }
